8550bb08d2006c126b8e8446efd5e46c980d5fac
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-09-30 19:47:05 +0200

    ajout de sryle et animation

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,11 +1,61 @@
+import { useState } from 'react';
 import './Menu.css';
+import { IonIcon } from '@ionic/react';
+import { 
+  addOutline, 
+  homeOutline, 
+  personOutline, 
+  settingsOutline, 
+  mailOutline, 
+  videocamOutline, 
+  keyOutline, 
+  gameControllerOutline, 
+  cameraOutline 
+} from 'ionicons/icons';
 
 const Menu = () => {
-    return (
-        <div className="menu">
-            <p>X</p>
-        </div>
-    );
+  const [isActive, setIsActive] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const icons = [
+    homeOutline,
+    personOutline,
+    settingsOutline,
+    mailOutline,
+    videocamOutline,
+    keyOutline,
+    gameControllerOutline,
+    cameraOutline,
+  ];
+
+  return (
+    <ul className={`menu ${isActive ? 'active' : ''}`}>
+      <div className={`toggle ${isActive ? 'active' : ''}`} onClick={toggleMenu}>
+        <IonIcon icon={addOutline} />
+      </div>
+      {icons.map((icon, index) => (
+        <li
+          key={index}
+          style={{ '--i': index }}
+          className={index === activeIndex ? 'active' : ''}
+          onClick={() => handleItemClick(index)}
+        >
+          <a href="#">
+            <IonIcon icon={icon} />
+          </a>
+        </li>
+      ))}
+      <div className="indicator"></div>
+    </ul>
+  );
 };
 
 export default Menu;
