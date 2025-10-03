a2b1ac4a44bdb07784df4eafff9d81de866b31b9
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:36:39 +0200

    page Video

---
+++ src/Pages/Video.jsx
@@ -0,0 +1,10 @@
+const Video = () => {
+  return (
+    <section>
+      <h1>Présentation vidéo</h1>
+      <p>Intégration prochaine d’une vidéo personnelle.</p>
+    </section>
+  );
+};
+
+export default Video;
