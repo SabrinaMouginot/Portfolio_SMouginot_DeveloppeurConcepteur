628be87a839a16a0286fc40c3eeff5983e4d14d7
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:37:03 +0200

    component card project

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const ProjectCard = ({ title, description, link }) => {
+  return (
+    <div className="project-card">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={link} target="_blank" rel="noreferrer">Voir le projet</a>
+    </div>
+  );
+};
+
+ProjectCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default ProjectCard;
