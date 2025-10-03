aed3055c9dbcae10d316a38274222b4ed97610dc
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:35:51 +0200

    page Hobbies

---
+++ src/Pages/Hobbies.jsx
@@ -0,0 +1,10 @@
+const Hobbies = () => {
+  return (
+    <section>
+      <h1>Mes hobbys</h1>
+      <p>Ici tu pourras partager tes passions : jeux, couture, lecture, etc.</p>
+    </section>
+  );
+};
+
+export default Hobbies;
