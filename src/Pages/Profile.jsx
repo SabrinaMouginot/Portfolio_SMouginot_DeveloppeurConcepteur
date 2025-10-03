7770d05f6ea8ea7589ab0d1ef0c0fdbe58b92ca9
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:33:24 +0200

    component Profile

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <section>
+      <h1>Mon Profil</h1>
+      <p>Voici une présentation personnelle à compléter.</p>
+    </section>
+  );
+};
+
+export default Profile;
