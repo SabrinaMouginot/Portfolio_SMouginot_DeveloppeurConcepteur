1999254f5b233ec5b48b88854453bfdad9a34af9
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:35:38 +0200

    page Keys

---
+++ src/Pages/Keys.jsx
@@ -0,0 +1,10 @@
+const Keys = () => {
+  return (
+    <section>
+      <h1>Les clefs de l’entreprise</h1>
+      <p>Accédez à mon autre site : <a href="https://pixyplanet.fr" target="_blank" rel="noreferrer">PixyPlanet.fr</a></p>
+    </section>
+  );
+};
+
+export default Keys;
