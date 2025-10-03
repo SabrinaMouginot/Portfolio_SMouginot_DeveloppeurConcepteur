80dfb49de0b152bc1b2ce6760a9b72b76b947d88
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-10-03 10:35:23 +0200

    page Contact

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <section>
+      <h1>Contact</h1>
+      <p>Formulaire ou informations de contact à venir.</p>
+    </section>
+  );
+};
+
+export default Contact;
